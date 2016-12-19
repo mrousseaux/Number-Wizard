2791ba9c4c084fae6847e9e5ad884b1d34aeddd2
Matthew Rousseaux <matthew@rousseaux.ca>
2016-12-19 14:59:36 -0600

    Menu Load Script
    MenuManager object created. MenuManager Script created, and logging.

---
+++ Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManagerScript : MonoBehaviour {
+	public void menuLoad(string name){
+		Debug.Log("Menu Loaded: " +name);
+	}
+
+	public void quitReturn(){
+		Debug.Log("Game Quit.");
+	}
+}
