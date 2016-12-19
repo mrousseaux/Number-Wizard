8646c5b352270b75721a82f2201e3d8f1b707c1c
Matthew Rousseaux <matthew@rousseaux.ca>
2016-12-19 16:50:44 -0600

    Game system implemented
    adapted NumberWizard logic code to provide methods and public variables
    for Unity. Attached to appropriate buttons and text.

--- Assets/Scripts/MenuManagerScript.cs
+++ Assets/Scripts/MenuManagerScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class MenuManagerScript : MonoBehaviour {
 	public void menuLoad(string name){
-		Debug.Log("Menu Loaded: " +name);
+		Debug.Log("Menu/Level Loaded: " +name);
+		Application.LoadLevel (name);
 	}
 
 	public void quitReturn(){
 		Debug.Log("Game Quit.");
+		Application.Quit ();
 	}
 }
--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,52 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
+	public Text guessText;
+	int maxGuesses = 10;
 	int max;
 	int min;
-	int guess = 500;
+	int guess;
 	// Use this for initialization
 	void Start () {
 		StartGame ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.Return)) {
-			print ("I won!!, I are smart!");
-			StartGame ();
-		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			print ("Higher eh?");
-			min = guess;
-			NextGuess();
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			print ("Lower you say?");
-			print ("Higher eh?");
-			max = guess;
-			NextGuess();
-		}
+	// Interaction methods
+	public void higherGuess(){
+		min = guess;
+		NextGuess();
+		Debug.Log ("The number is higher");
+	}
+	public void lowerGuess(){
+		max = guess;
+		NextGuess();
+		Debug.Log ("The number is lower");
 	}
 
-	// Functions
+	// Gameplay Methods
 	void StartGame () {
 		max = 1001;
 		min = 1;
 		guess = 500;
-
-		print ("------------------------");
-		print ("");
-		print ("Welcome to Number Wizard");
-		print ("Pick a number but DON'T tell me");
-		
-		print ("The highest number you can choose is " + max);
-		print ("The lowest number you can pick is " + min);
-		
-		print ("Is the number high or lower than " + guess + "?");
-		print ("Up = higher, Down = lower, Return = equals.");
 		max = max + 1;
 	}
 	void NextGuess(){
 		guess = (max + min)/2;
-		print ("Higher or lower than " + guess);
+		guessText.text = guess.ToString ();
+		maxGuesses = maxGuesses-1;
+		if (maxGuesses <= 0){
+			Application.LoadLevel("Win");
+		}
 	}
 }
